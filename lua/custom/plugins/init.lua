e53fae00e41f3964b3358d468a87bc62e304c802
Tales Carvalho <tales-h-carvalho@hotmail.com>
2025-12-20 13:31:56 -0700

    custom plugins: auto-session and gitlinker

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { -- Gitlinker: <leader>gy to copy permalink of remote git in current line
+    'ruifm/gitlinker.nvim',
+    opts = {},
+  },
+  { -- Auto-session
+    'rmagatti/auto-session',
+    keys = {
+      { '<leader>wr', '<cmd>AutoSession search<CR>', desc = 'Session search' },
+      { '<leader>ws', '<cmd>AutoSession save<CR>', desc = 'Save session' },
+      { '<leader>wa', '<cmd>AutoSession toggle<CR>', desc = 'Toggle autosave' },
+    },
+    opts = {
+      suppressed_dirs = { '~/', '~/Downloads', '/' },
+    },
+  },
+}
